6e4e33994fc209c8c9b06d74b9c8329e60093c8d
Justyna <justynapanekk@gmail.com>
2023-11-17 00:01:38 +0100

    Add successful login path

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,9 +1,21 @@
 describe('Visit Langing Page', () => {
+  beforeEach(() => {
+    cy.unregisterServiceWorkers();
+  });
+
   it('passes', () => {
     cy.visit('https://www.saucedemo.com')
     cy.get('.login_logo').contains('Swag Labs')
 
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+
+    cy.get('[data-test="login-button"]').click()
+
+    cy.get('span.title').contains('Products')
+
+    cy.get('button#react-burger-menu-btn').click()
+    cy.get('a#logout_sidebar_link').click()
+    cy.get('.login_logo').contains('Swag Labs')
   })
 })
