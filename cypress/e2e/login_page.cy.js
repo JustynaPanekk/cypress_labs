47b247df4252c695253c35489e1c43cdf2c29c90
Justyna Panek <148354531+JustynaPanekk@users.noreply.github.com>
2023-11-27 22:16:12 +0100

    add e2e test to cover locked log in user (#3)

--- cypress/e2e/login_page.cy.js
+++ cypress/e2e/login_page.cy.js
@@ -1,9 +1,9 @@
-describe('Visit Langing Page', () => {
+describe('User logs in', () => {
   beforeEach(() => {
     cy.unregisterServiceWorkers();
   });
 
-  it('passes', () => {
+  it('is able to log in giving valid credentials', () => {
     cy.visit('https://www.saucedemo.com')
     cy.get('.login_logo').contains('Swag Labs')
 
@@ -17,5 +17,17 @@ describe('Visit Langing Page', () => {
     cy.get('button#react-burger-menu-btn').click()
     cy.get('a#logout_sidebar_link').click()
     cy.get('.login_logo').contains('Swag Labs')
+  })
+
+  it('is unable to log in with locked account', () => {
+    cy.visit('https://www.saucedemo.com')
+    cy.get('.login_logo').contains('Swag Labs')
+
+    cy.get('[data-test="username"]').type('locked_out_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+
+    cy.get('[data-test="login-button"]').click()
+
+    cy.get('[data-test="error"]').contains('Epic sadface: Sorry, this user has been locked out.')
   })
 })
