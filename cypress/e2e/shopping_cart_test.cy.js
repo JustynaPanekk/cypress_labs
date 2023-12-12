29dfbbf380c85548c18649ac2af83de46c84e4e5
Justyna <justynapanekk@gmail.com>
2023-12-12 13:58:19 +0100

    remove items from the shopping cart

--- cypress/e2e/shopping_cart_test.cy.js
+++ cypress/e2e/shopping_cart_test.cy.js
@@ -30,10 +30,35 @@ describe('Verify product list page', () => {
       cy.get('.inventory_list>.inventory_item').each((product, index) => {
         cy.wrap(product).find('button.btn').click();
         cy.get('.shopping_cart_badge').should('have.text', `${index + 1}`)
-    
-
-
       })
      
+    })
+
+    it('verify that you can remove items from the shopping cart', () => {
+      cy.visit('https://www.saucedemo.com')
+      cy.get('.login_logo').contains('Swag Labs')
+  
+      cy.get('[data-test="username"]').type('standard_user')
+      cy.get('[data-test="password"]').type('secret_sauce')
+  
+      cy.get('[data-test="login-button"]').click()
+
+      cy.get('.shopping_cart_link').click()
+
+      cy.get('.inventory_item').should('not.exist')
+
+      cy.get('button#continue-shopping').click()
+
+      cy.get('#add-to-cart-sauce-labs-backpack').click();
+
+      cy.get('.shopping_cart_badge').should('have.text', '1');
+
+      cy.get('.shopping_cart_link').click()
+
+      cy.get("#item_4_title_link").should("be.visible");
+
+      cy.get('button#remove-sauce-labs-backpack').click();
+
+      cy.get('.inventory_item').should('not.exist');
     })
   })
